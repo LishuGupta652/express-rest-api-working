ce4d55be16ba2789f2174882fd82a9c347a73014
lishu <lishugupta652@gmail.com>
2021-11-17 17:59:05 +0530

    spefic post added

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,47 @@
 const router = require("express").Router();
 const Post = require("../models/postModel");
 
-router.get("/", (req, res) => {
-  console.log("Get is working fine");
+// Geet all post
+router.get("/", async (req, res) => {
+  try {
+    const allPost = await Post.find();
+    res.send(allPost);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
+// Submit a post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  //   post
+  //     .save()
+  //     .then((result) => {
+  //       console.log(result);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+
+  // Method 02
+  try {
+    const data = await post.save();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Find a specific post
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.send(post);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 module.exports = router;
