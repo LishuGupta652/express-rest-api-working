069433e2c80cda2aed7710f7ff8ae626d71ac10a
lishu <lishugupta652@gmail.com>
2021-12-15 20:36:14 +0530

    Adding url shortner: Mongoose model

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 
-const shortUrlSchema = mongoose.Schema({});
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+  },
+});
