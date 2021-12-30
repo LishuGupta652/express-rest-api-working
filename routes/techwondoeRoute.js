469aae6239572571660c4c71b6d5dbc9930d1f37
lishu <lishugupta652@gmail.com>
2021-12-30 19:37:06 +0530

    techwondoe form > Router

---
+++ routes/techwondoeRoute.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Data = require("../models/techwondoeModel");
+
+router.post("/", async (req, res) => {
+  const newPost = new Data(req.body);
+
+  try {
+    const data = await newPost.save();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const allPost = await Data.find();
+    res.send(allPost);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
